f689365f94b66effbf210e448e65a5dd456c5c89
Ayush, Rastogi <rastogi.nitt@gmail.com>
2018-08-17 19:16:49 +0000

    building list

--- LinkedList.cxx
+++ LinkedList.cxx
@@ -1,4 +1,24 @@
+#include <iostream>
+
+struct Node
+{
+  Node* next;
+  int data;
+};
+
+void insertAtHead( Node* head, int data )
+{
+  if( head == NULL )
+    {
+      head = new Node();
+      head->next = NULL;
+      head->data = data;
+    }
+}
+
 int main()
 {
+  Node * head = NULL;
+  insertAtHead( head, 10 );
   return 0;
 }
