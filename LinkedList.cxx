10a3bc0d4448d2f3685d7e597c7794bd6003967a
Hello Brother <rastogi.nitt@gmail.com>
2018-08-22 05:02:02 +0000

    updating list

--- LinkedList.cxx
+++ LinkedList.cxx
@@ -6,19 +6,63 @@ struct Node
   int data;
 };
 
-void insertAtHead( Node* head, int data )
+/*
+This function is expected to 
+
+ */
+Node* insertAtEnd( Node* head, int data )
 {
+  Node* headToReturn = head;
+  
+  // Head is empty, there is no element in the list
   if( head == NULL )
     {
       head = new Node();
       head->next = NULL;
       head->data = data;
+
+      std::cout << "Inserted " << data << " at head, fist elment in list inserted" << std::endl;
+      return head;
     }
+  
+  // There are already some elements in the List, so let's find out the end of list
+  // Then we add the new element at the end of the list
+
+  // We are going to modify the head, as we traverse it
+  // So we store the orignal <head> pointer so we can return it back
+  else
+    {
+      int count = 0;
+      while( head != NULL )
+	{
+	  head = head->next;
+	  count = count + 1;
+	}
+      head = new Node();
+      head->next = NULL;
+      head->data = data;
+
+      std::cout << "Inserted " << data << " at head, size of List is " << count + 1 << std::endl;
+    }
+  
+  return headToReturn;
+}
+
+void printList( Node* head )
+{
+  while( head != NULL )
+    {
+      std::cout << head->data << "->";
+      head = head->next;
+    }
+  std::cout << "--|" << std::endl; 
 }
 
 int main()
 {
   Node * head = NULL;
-  insertAtHead( head, 10 );
+  head = insertAtEnd( head, 10 );
+  head = insertAtEnd( head, 20 );
+  printList( head );
   return 0;
 }
